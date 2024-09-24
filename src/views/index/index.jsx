9abfa1f7c502c96f9dadad1151f202f122606014
joshdev12 <joshuaajayi012@gmail.com>
2024-09-24 15:25:56 +0100

    'Image'

--- src/views/index/index.jsx
+++ src/views/index/index.jsx
@@ -1,8 +1,24 @@
 const IndexPage = () => {
   return (
     <div>
-      <h1> Hello World </h1>
-      <p>SUI Project Landing Page</p>
+      <div>
+        <section className="image">
+          <img src="" alt="" />          
+        </section>
+        <section className="content container p-5">
+          <h2>
+            BUILD A SECURE, PRIVATE, AND COMMUNITY-DRIVEN EMAIL PLATFORM WHERE USERS HAVE FULL CONTROL OVER THEIR DATA AND IDENTITY.
+          </h2> 
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi, debitis deserunt. Natus accusamus qui, voluptatem explicabo, unde iusto quidem laudantium inventore similique tenetur atque deserunt optio id adipisci aperiam cum!
+            Alias officia nulla mollitia quaerat eligendi sapiente voluptates amet laudantium pariatur laborum blanditiis rerum voluptas consectetur delectus, nam nisi molestias cum omnis ut, nesciunt eos molestiae quibusdam? Exercitationem, rerum quasi.
+            Incidunt velit porro rem mollitia consequuntur nihil eum nam laboriosam! Iste, magnam? Minima, numquam quos unde libero ducimus dolorum ad sapiente delectus iure dolorem, eveniet ipsum aut ab ea provident!
+            Fugiat impedit veniam suscipit sit quae sunt! Error sunt molestiae fuga vero id, nam ex non nobis cumque expedita pariatur culpa asperiores porro. Dolorum sint beatae debitis molestiae? Eligendi, sunt!
+            Omnis fugiat sed necessitatibus consequuntur veritatis beatae quis dolorem sunt. Unde animi necessitatibus rem delectus, enim eius, iusto odit veniam nesciunt error dignissimos incidunt minus maiores, dicta laudantium porro ullam?
+          </p>         
+        </section>
+      </div>
+     
     </div>
   )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,14 @@ export default {
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
+
   theme: {
+    screens: {
+      sm: '480px',
+      md: '768px',
+      lg: '976px',
+      xl: '1440px',
+    },
     extend: {},
   },
   plugins: [],
