6539be66ce1c98df82590970c4808ca8d28caada
ibrahim abdulsalam <ibsalam24@gmail.com>
2024-09-28 11:27:26 +0100

    modal pages

--- src/AppRoutes.jsx
+++ src/AppRoutes.jsx
@@ -1,12 +1,13 @@
 import { Route, Routes } from "react-router-dom"
 import Layout from "./components/layout/Layout"
 import IndexPage from "./views/index"
+import TestingPage from "./views/TestingModal"
 
 export default function AppRoutes() {
   return (
     <Routes>
       <Route path="/" element={<Layout />}>
-        <Route path="" element={<IndexPage />} />
+        <Route path="" element={<TestingPage />} />
       </Route>
 
       {/* Not found screen */}
---
+++ src/views/TestingModal.jsx
@@ -0,0 +1,14 @@
+import React, { useState } from "react";
+import Modal from "../components/Modal/Modal";
+
+function HomePage() {
+  const [onClick,setOnClick] = useState(false)
+  return (
+    <div className='bg-white min-h-screen flex'>
+     <button onClick={()=>setOnClick(!onClick)}>button</button>
+ { onClick ?<Modal setOnClick={setOnClick}/> :''}   
+    </div>
+  );
+}
+
+export default HomePage;
