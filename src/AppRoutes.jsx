22bd56adfe15f42b336379848fa4cdc6c09eb4a5
Abdol <Wabdullahi565@gmail.com>
2024-10-01 05:54:17 -0700

    Email-State

--- src/AppRoutes.jsx
+++ src/AppRoutes.jsx
@@ -4,6 +4,7 @@ import IndexPage from "./views/index"
 import LoginPage from "./views/login"
 import SignUpPage from "./views/signup"
 import EmailList from "./views/Email-list"
+import EmailState from "./views/state"
 import AuthLayout from "./components/layout/AuthLayout"
 
 export default function AppRoutes() {
@@ -12,6 +13,7 @@ export default function AppRoutes() {
       <Route path="/" element={<Layout />}>
         <Route path="" element={<IndexPage />} />
         <Route path="email" element={<EmailList/>} />
+        <Route path="active" element={<EmailState/>} />
         <Route path="auth" element={<AuthLayout />}>
           <Route path="create-account" element={<SignUpPage />} />
           <Route path="login" element={<LoginPage />} />
