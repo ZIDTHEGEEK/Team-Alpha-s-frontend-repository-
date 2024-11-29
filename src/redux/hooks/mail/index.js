9bd035be489e77c38f68b496d1d7597585f94c7a
joshdev12 <joshuaajayi012@gmail.com>
2024-11-29 02:52:16 -0800

    refactor

---
+++ src/redux/hooks/mail/index.js
@@ -0,0 +1,21 @@
+import { useMutation } from "@tanstack/react-query"
+// import { sendMail, deleteMail } from "../../../services/mail.service"
+import { deleteMail, sendMail } from '../../../services/mail.service'
+
+
+export const useSendMaillQuery = () =>{
+    return useMutation({
+        mutationFn: () => sendMail () 
+    })
+}
+
+
+export const useDeleteMaillQuery = () =>{
+    return useMutation({
+        mutationFn: () => deleteMail()
+    })
+}
+
+
+
+
